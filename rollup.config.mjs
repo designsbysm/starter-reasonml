39ead5a3e868bb5b9d5378e29e01b60fdcbf27f5
Scott A. Matthews <scott@designsbysm.com>
2024-08-02 00:42:33 -0700

    format files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,14 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'server/src/server/index.mjs',
+  input: "server/src/server/index.mjs",
   output: {
-    file: './dist-server/index.js',
-    format: 'es',
+    file: "./dist-server/index.js",
+    format: "es",
   },
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    production && terser(),
-  ]
+  plugins: [nodeResolve(), commonjs(), production && terser()],
 };
